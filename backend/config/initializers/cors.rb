5329e5bb0fa50fc47c61b46319c7625d5a438d32
Dino-HodzicTumpic <dino.hodzic-tumpic@fer.unizg.hr>
2026-05-18 15:11:44 +0200

    Update cors config for prod

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'https://your-app.vercel.app'
+    origins 'http://localhost:5173', 'https://rails-react-cat-app.vercel.app'
 
     resource '*',
              headers: :any,
